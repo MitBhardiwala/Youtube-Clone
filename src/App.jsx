c7f411a99c6862af5a33687d1bd42dfa93ac6fe9
Mit0921 <meetbhardiwala@gmail.com>
2024-07-21 22:22:01 +0530

    Add vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 
 import "./App.css";
-// import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import { Box } from "@mui/material";
-import { HashRouter as Router, Route, Switch,useParams } from 'react-router-dom';
 import {Navbar,Feed,VideoDetail,ChannelDetail,SearchFeed} from "./components"
-
+import { HashRouter } from "react-router-dom";
 
 function App() {
   
 
   return (
     <>
-      <Router>
+      <BrowserRouter>
       
         <Box sx={{backgroundColor:"black"}}>
           <Navbar/>
@@ -22,7 +21,7 @@ function App() {
               <Route path="/search/:searchTerm" exact element={<SearchFeed/>}></Route>
             </Routes>
         </Box>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
