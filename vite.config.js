f60234a8396fe5a0b7ece1888453350658396053
Mit0921 <meetbhardiwala@gmail.com>
2024-07-29 20:13:36 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  plugins: [react()],
-  define: {
-    "process.env": process.env,
-    VITE_REACT_APP_RAPID_API_KEY: process.env.VITE_REACT_APP_RAPID_API_KEY,
-  },
+  plugins: [react()]
 })
