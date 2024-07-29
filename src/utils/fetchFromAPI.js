27a1d75ea39d04b8c5402dccad6bcfd0d701ce5e
Mit0921 <meetbhardiwala@gmail.com>
2024-07-29 19:43:10 +0530

    updated fetchFromAPI

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -11,7 +11,7 @@ const options = {
     maxResults: '50',
   },
   headers: {
-    'x-rapidapi-key': '71158d418amsh5a224da5aafb1b4p1b2094jsn7cc73a0b9d66',
+    'x-rapidapi-key': import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
     'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
   }
 };
@@ -19,6 +19,7 @@ const options = {
 export const fetchFromAPI = async (url) => {
   try {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+   
     return data;
   } catch (error) {
     console.error("Error fetching data from API:", error);
